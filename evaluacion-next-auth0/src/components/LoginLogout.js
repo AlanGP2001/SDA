0d39078a8ad3585c295e9a676c59a1cf67a13325
AlanGP2001 <93557645+AlanGP2001@users.noreply.github.com>
2023-06-23 13:15:50 -0600

    Update LoginLogout.js

--- evaluacion-next-auth0/src/components/LoginLogout.js
+++ evaluacion-next-auth0/src/components/LoginLogout.js
@@ -3,28 +3,59 @@ import { useUser } from '@auth0/nextjs-auth0/client'
 import styles from '../../styles/Home.module.css'
 
 const LoginLogout = () => {
+  const { user, error, isLoading } = useUser();
 
-   const { user, error, isLoading } = useUser();
+  if (isLoading) {
+    return <div>Cargando...</div>;
+  }
 
-    if(error) return <div>{error.message}</div>
-    if (isLoading) return <div>Loading...</div>
-    if (user) console.log(user)
+  if (error) {
+    return <div>{error.message}</div>;
+  }
 
+  if (user) {
     return (
-        <div className={styles.grid}>
-            {!user && (
-                <a href="/api/auth/login" className={styles.card}>
-                    <h2>Iniciar Sesion &rarr;</h2>
-                </a>
-            )}
-
-            {user && (
-                <a href="/api/auth/logout" className={styles.card}>
-                    <h2>Cerrar Sesion &rarr;</h2>
-                </a>
-            )}
-        </div>
+      <div>
+        <p>Conectado como: {user.email}</p>
+        <a href="/api/auth/logout">
+          <a>Cerrar sesión</a>
+        </a>
+      </div>
     );
+  }
+
+  return (
+    <div>
+      <p>Por favor inicie sesión para continuar.</p>
+      <a href="/api/auth/login">
+        <a>Acceso</a>
+      </a>
+    </div>
+  );
 }
+// const LoginLogout = () => {
+
+//    const { user, error, isLoading } = useUser();
+
+//     if(error) return <div>{error.message}</div>
+//     if (isLoading) return <div>Loading...</div>
+//     if (user) console.log(user)
+
+//     return (
+//         <div className={styles.grid}>
+//             {!user && (
+//                 <a href="/api/auth/login" className={styles.card}>
+//                     <h2>Iniciar Sesion &rarr;</h2>
+//                 </a>
+//             )}
+
+//             {user && (
+//                 <a href="/api/auth/logout" className={styles.card}>
+//                     <h2>Cerrar Sesion &rarr;</h2>
+//                 </a>
+//             )}
+//         </div>
+//     );
+// }
 
 export default LoginLogout;
